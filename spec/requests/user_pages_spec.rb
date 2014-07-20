13ebad1a0c849ce905edbf2c3af55dac8c0803bb
butterflyhong <butterflyhong@gmail.com>
2014-07-20 23:08:35 +0800

    add delete function

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,6 +2,32 @@ require 'spec_helper'
 
 describe "User pages" do
 	subject { page }
+
+	describe "index" do
+	  before do
+	  	sign_in FactoryGirl.create(:user)
+	  	FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+	  	FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+	  	visit users_path
+	  end
+
+	  it { should have_title('All users') }
+	  it { should have_content('All users') }
+
+	  describe "pagination" do
+	  	before(:all) { 30.times { FactoryGirl.create(:user)} }
+	  	after(:all) { User.delete_all }
+
+	  	it { should have_selector('div.pagination') }
+
+		it "should list each user" do
+		  User.all.each do |user|
+		  	expect(page).to have_selector('li', text: user.name)
+		  end
+		end
+	  end
+	end
+
 	describe "signup" do
 		before { visit signup_path }
 		let(:submit) { "Create my account" }
@@ -48,7 +74,10 @@ describe "User pages" do
 
 	describe "edit" do
 		let(:user) { FactoryGirl.create(:user) }
-		before { visit edit_user_path(:user) }
+		before do
+		  sign_in user	
+		  visit edit_user_path(user)
+		end
 
 		describe "page" do
 			it { should have_content("Update your profile") }
@@ -59,6 +88,24 @@ describe "User pages" do
 		describe "with invalid information" do
 			before { click_button "Save changes" }
 			it { should have_content("error") }
+		end
+
+		describe "with valid information"　do
+			let(:new_name) { "New name" }
+			let(:new_email) { "new@example.com" }
+			before do
+				fill_in "Name",          with: new_name
+				fill_in "Email",         with: new_email
+				fill_in "Password"       with: user.password
+				fill_in "Confirm Password" with: user.password
+				click_button "Save changes" 
+			end
+
+			it { should have_title(new_name) }
+			it { should have_selector('div.alert.alert-success') }
+			it { should have_link('Sign out', href: signout_path) }
+			specify { expect(user.reload.name).to eq new_name }
+			specify { expect(user.reload.email).to eq new_email }
 		end
 	end
 end
